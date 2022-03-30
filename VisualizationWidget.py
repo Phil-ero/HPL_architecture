5d7b1d52a1d87945db748227cddf5b03a5a22bc7
spacewolfXfr <maelfeurgard@gmail.com>
2022-03-30 18:37:47 +0200

    Added base design for visualization widget

--- VisualizationWidget.py
+++ VisualizationWidget.py
@@ -1,2 +1,21 @@
-from PyQt5.QtWidgets import QWidget, QGroupBox, QBoxLayout
+from PyQt5.QtWidgets import QWidget, QGroupBox, QGridLayout
 from PyQt5.QtCore import Qt
+from PyQt5 import QtCore
+import typing
+
+class VisualizationWidget(QWidget):
+    def __init__(self, parent: typing.Optional['QWidget'] = ..., flags: typing.Union[QtCore.Qt.WindowFlags, QtCore.Qt.WindowType] = ...) -> None:
+        super().__init__(parent, flags)
+        
+        layout = QGridLayout()
+        
+        vueCote = QWidget()
+        vueDessus = QWidget()
+        vue3D = QWidget()
+        
+        layout.addWidget(vueCote,0,0)
+        layout.addWidget(vueDessus,1,1)
+        layout.addWidget(vue3D,1,0,1,2)
+        
+        
+        
--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGroupBox, QBoxLayout
+from unittest import result
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGroupBox, QBoxLayout, QGridLayout, QSlider, QLabel
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette, QColor
 
 import ParametersWidget, VisualizationWidget, ResultsWidget
 
 app = QApplication(sys.argv)
+
+class Color(QWidget):
+    
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,25 +26,51 @@ class MainWindow(QMainWindow):
 
         mainWidget = QWidget()
 
-        layout = QBoxLayout(QBoxLayout.LeftToRight)
+        #layout = QBoxLayout(QBoxLayout.LeftToRight)
+        layout = QGridLayout()
 
         self.setWindowTitle("Test App")
-        leftFrame = QGroupBox("Parameters", self)
-        leftFrame.setAlignment(Qt.AlignLeft)
+        paramFrame = QGroupBox("Parameters", self)
+        paramFrame.setAlignment(Qt.AlignCenter)
         
-        midFrame = QGroupBox("Visualisation",self)
-        midFrame.setAlignment(Qt.AlignCenter)
+        paramlayout = QBoxLayout(QBoxLayout.TopToBottom)
+        paramlayout.addWidget(QLabel("Param1"))
+        paramlayout.addWidget(QSlider(Qt.Horizontal))
+        paramlayout.addWidget(QLabel("Param2"))
+        paramlayout.addWidget(QSlider(Qt.Horizontal))
+        paramlayout.addWidget(QLabel("Param3"))
+        paramlayout.addWidget(QSlider(Qt.Horizontal))
         
-        rightFrame = QGroupBox("Results",self)
-        rightFrame.setAlignment(Qt.AlignRight)
+        paramFrame.setLayout(paramlayout)
         
-        layout.addWidget(leftFrame)
-        layout.addWidget(midFrame)
-        layout.addWidget(rightFrame)
+        visFrame = QGroupBox("Visualisation",self)
+        visFrame.setAlignment(Qt.AlignCenter)
+        
+        vislayout = QBoxLayout(QBoxLayout.TopToBottom)
+        vislayout.addWidget(Color(QColor(255,0,0)))
+        
+        visFrame.setLayout(vislayout)
+        
+        resultsFrame = QGroupBox("Results",self)
+        resultsFrame.setAlignment(Qt.AlignCenter)
+        
+        resulstlayout = QBoxLayout(QBoxLayout.LeftToRight)
+        resulstlayout.addWidget(Color(QColor(0,0,255)))
+        resulstlayout.addWidget(Color(QColor(255,255,255)))
+        resulstlayout.addWidget(Color(QColor(255,0,0)))
+        
+        resultsFrame.setLayout(resulstlayout)
+        
+        
+        layout.addWidget(paramFrame,0,0)
+        layout.addWidget(visFrame,0,1)
+        layout.addWidget(resultsFrame,1,0,1,2)
+        
         
         mainWidget.setLayout(layout)
         
         self.setCentralWidget(mainWidget)
+
         
 
 window = MainWindow()
