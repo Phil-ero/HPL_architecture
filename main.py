ebe0dc052b82c4b0fcae00795de642c8a381a72f
spacewolfXfr <maelfeurgard@gmail.com>
2022-04-27 18:00:00 +0200

    Changed from prototype to project begin

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGroupBox, QBoxLayout, QGridLayout, QSlider, QLabel
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPalette, QColor
+import typing
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QTabWidget, QMenuBar, QLabel
+import PyQt5.QtCore as QtCore
+from PyQt5.QtGui import QPalette, QColor, QRgba64
 
 import ParametersWidget, VisualizationWidget, ResultsWidget
-
-app = QApplication(sys.argv)
+from TabContent import TabContent
 
 class Color(QWidget):
-    
-    def __init__(self, color):
-        super(Color, self).__init__()
+    def __init__(self,color:typing.Union[str,QRgba64]) -> None:
+        super().__init__()
         self.setAutoFillBackground(True)
 
         palette = self.palette()
@@ -20,60 +19,39 @@ class Color(QWidget):
         self.setPalette(palette)
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def _createMenuBar(self) -> None:
+        menuBar = QMenuBar(self)
+        self.setMenuBar(menuBar)
+        
+        fileMenu = menuBar.addMenu("&File")
+        editMenu = menuBar.addMenu("&Edit")
+        helpMenu = menuBar.addMenu("&Help")
+
+        
+    def _createTabWidget(self) -> None:
+        tabWidget = QTabWidget()
+        tabWidget.addTab(TabContent(self),"Tab 1")
+        tabWidget.addTab(TabContent(self),"Tab 2")
+        tabWidget.addTab(TabContent(self),"Tab 3")
+        self.centralWidget = tabWidget
+        #self.centralWidget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        self.setCentralWidget(self.centralWidget)
+
+    
+    def __init__(self) -> None:
         super().__init__()
-
-        mainWidget = QWidget()
-
-        #layout = QBoxLayout(QBoxLayout.LeftToRight)
-        layout = QGridLayout()
-
-        self.setWindowTitle("Test App")
-        paramFrame = QGroupBox("Parameters", self)
-        paramFrame.setAlignment(Qt.AlignCenter)
+        self.setWindowTitle("HPL : Solar Pannel Simulator")
+        self.resize(400, 200)
         
-        paramlayout = QBoxLayout(QBoxLayout.TopToBottom)
-        paramlayout.addWidget(QLabel("Param1"))
-        paramlayout.addWidget(QSlider(Qt.Horizontal))
-        paramlayout.addWidget(QLabel("Param2"))
-        paramlayout.addWidget(QSlider(Qt.Horizontal))
-        paramlayout.addWidget(QLabel("Param3"))
-        paramlayout.addWidget(QSlider(Qt.Horizontal))
-        
-        paramFrame.setLayout(paramlayout)
-        
-        visFrame = QGroupBox("Visualisation",self)
-        visFrame.setAlignment(Qt.AlignCenter)
-        
-        vislayout = QBoxLayout(QBoxLayout.TopToBottom)
-        vislayout.addWidget(Color(QColor(255,0,0)))
-        
-        visFrame.setLayout(vislayout)
-        
-        resultsFrame = QGroupBox("Results",self)
-        resultsFrame.setAlignment(Qt.AlignCenter)
-        
-        resulstlayout = QBoxLayout(QBoxLayout.LeftToRight)
-        resulstlayout.addWidget(Color(QColor(0,0,255)))
-        resulstlayout.addWidget(Color(QColor(255,255,255)))
-        resulstlayout.addWidget(Color(QColor(255,0,0)))
-        
-        resultsFrame.setLayout(resulstlayout)
-        
-        
-        layout.addWidget(paramFrame,0,0)
-        layout.addWidget(visFrame,0,1)
-        layout.addWidget(resultsFrame,1,0,1,2)
-        
-        
-        mainWidget.setLayout(layout)
-        
-        self.setCentralWidget(mainWidget)
-
+        self._createMenuBar()
+        self._createTabWidget()
         
 
-window = MainWindow()
-window.show()
-
-# Start the event loop.
-app.exec()
+def main():
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec_())
+    
+if __name__ == "__main__":
+    main()
