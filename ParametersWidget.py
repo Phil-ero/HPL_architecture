35a1618eb2e896da5a56657d3b44b5a601101088
spacewolfXfr <maelfeurgard@gmail.com>
2022-04-28 11:23:24 +0200

    Made interactive Slider + Textbox

--- ParametersWidget.py
+++ ParametersWidget.py
@@ -7,23 +7,42 @@ from Section import Section
 
 #################### Box for each parameter ####################
 
-
 class HBoxSlider(QWidget):
     def __init__(self, minVal: int, maxVal: int, stepVal: int, parent: typing.Optional['QWidget'] = None) -> None:
         super().__init__(parent)
 
         layout = QGridLayout(self)
 
-        slider = QSlider(Qt.Orientation.Horizontal, self)
-        slider.setRange(minVal,maxVal)
-        slider.setSingleStep(stepVal)
-        slider.setValue(minVal + (maxVal-minVal)//2)
-        layout.addWidget(slider,0,0,1,2)
+        self.slider = QSlider(Qt.Orientation.Horizontal, self)
+        self.slider.setRange(minVal,maxVal)
+        self.slider.setSingleStep(stepVal)
+        self.slider.setValue(minVal + (maxVal-minVal)//2)
+        self.slider.valueChanged.connect(self._SliderUpdated)
+        layout.addWidget(self.slider,0,0,1,2)
         
-        textbox = QLineEdit(str(minVal + (maxVal-minVal)//2),self)
-        layout.addWidget(textbox,0,2)
+        self.textbox = QLineEdit(str(minVal + (maxVal-minVal)//2),self)
+        self.textbox.setMaxLength(len(str(maxVal)))
+        text_fontMetrics = self.textbox.fontMetrics()
+        self.textbox.setFixedWidth(text_fontMetrics.maxWidth()*self.textbox.maxLength())
+        self.textbox.returnPressed.connect(self._TextboxUpdated)
+        layout.addWidget(self.textbox,0,2)
         
         self.setLayout(layout)
+        
+    def _SliderUpdated(self):
+        val = self.slider.value()
+        self.textbox.setText(str(val))
+        
+    def _TextboxUpdated(self):
+        try:
+            val = int(self.textbox.text())
+        except ValueError:
+            val = self.slider.value()
+            self.textbox.setText(str(val))
+            return
+        
+        self.slider.setValue(val)
+        
 
 #################### Parameters' stack ####################
 
@@ -61,7 +80,15 @@ def test():
         [Section("L1"), Section("L2"), Section("L3")], win))
     win.show()
     sys.exit(app.exec_())
+    
+def test_HBoxSlider():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setWindowTitle("HBoxSlider test window")
+    win.setCentralWidget(HBoxSlider(0,10,1,win))
+    win.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
-    test()
+    test_HBoxSlider()
